59097741124cf3389dd2ad4ed3dfcef1102454c1
samuel <mydifferentusername18@gmail.com>
2025-06-25 17:12:14 +0800

    did sthinsg

--- backend/file_manager.py
+++ backend/file_manager.py
@@ -11,3 +11,30 @@ def save_csv(df: pd.DataFrame, table_code):
 
 def load_csv(path):
     return pd.read_csv(path)
+
+def list_csvs():
+    files = [f for f in os.listdir("./tmp") if f.endswith(".csv")]
+    return {"csv files": files}
+
+def save_joined(df: pd.DataFrame, name):
+    dir = "./joined"
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    path = os.path.join(dir, f"{name}.csv")
+    df.to_csv(path, index=False)
+    return path
+
+
+def update():
+    l = list()
+    dir = "./tmp"
+    for f in os.listdir(dir):
+        if not f.endswith("joined.csv"):
+            l.append(f[0:-4])
+    return l
+
+def delete(name):
+    dir = "./tmp"
+    path = os.path.join(dir, f"{name}.csv")
+    os.remove(path)
+    return name
--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, UploadFile, Form
 from fastapi.responses import FileResponse, JSONResponse
 from client import fetch_table_data, fetch_metadata
 from cleaner import to_df
-from file_manager import save_csv, load_csv
+from file_manager import save_csv, load_csv, list_csvs, save_joined, update, delete
 from joiner import leftjoin_df, innerjoin_df, rightjoin_df
 import uvicorn
 
@@ -18,16 +18,69 @@ def list_tables(id):
 
 @app.post("/query")
 def query_table(id):
-    raw = fetch_table_data(id)
-    df = to_df(raw)
-    path = save_csv(df, id)
-    return FileResponse(path, media_type="text/csv", filename=f"{id}.csv")
+    try:
+        raw = fetch_table_data(id)
+        df = to_df(raw)
+        path = save_csv(df, id)
+        return FileResponse(path, media_type="text/csv", filename=f"{id}.csv")
+    except Exception as e:
+        return JSONResponse(status_code=500, content = {"error": str(e)})
 
-@app.post("preview")
+@app.post("/preview")
 def preview_table(id):
-    raw = fetch_table_data(id)
-    df = to_df(raw)
-    return JSONResponse(df.head(20).to_dict(orient="records"))
+    try:
+        raw = fetch_table_data(id)
+        df = to_df(raw)
+        df["Date"] = df["Date"].astype(str)
+        return JSONResponse(df.head(20).to_dict(orient="records"))
+    except Exception as e:
+        return JSONResponse(status_code=500, content = {"error": str(e)})
+
+@app.post("/join")
+def join_csvs(table1: str = Form(...), table2: str = Form(...), key: str = Form("Date"), name: str = Form(...)):
+    try:
+        df1 = load_csv(f"./tmp/{table1}.csv")
+        df2 = load_csv(f"./tmp/{table2}.csv")
+        joined = leftjoin_df(df1, df2, key)
+        path = save_csv(joined, f"{name}_joined")
+        return FileResponse(path, media_type="text/csv", filename=f"{name}.csv")
+    except Exception as e:
+        return JSONResponse(status_code=500, content = {"error": str(e)})
+
+@app.get("/list")
+def list_files():
+    try:
+        return list_csvs()
+    except Exception as e:
+        return JSONResponse(status_code=500, content = {"error": str(e)})
+    
+@app.post("/view")
+def view_files(id):
+    try:
+        df = load_csv(f"./tmp/{id}.csv")
+        df["Date"] = df["Date"].astype(str)
+        return JSONResponse(df.head(20).to_dict(orient="records"))
+    except Exception as e:
+        return JSONResponse(status_code=500, content = {"error": str(e)})
+
+@app.get("/update")
+def update_csv():
+    n = update()
+
+    for id in n:
+        raw = fetch_table_data(id)
+        df = to_df(raw)
+        path = save_csv(df, id)
+    
+    return JSONResponse({"number of files updated: ": len(n)})
+
+@app.post("/remove")
+def remove_file(name):
+    try:
+        n = delete(name)
+        return JSONResponse({"file deleted: ": f"{n}.csv"})
+    except Exception as e:
+        return JSONResponse(status_code=500, content = {"error": str(e)})
 
 
 if __name__ == "__main__":
